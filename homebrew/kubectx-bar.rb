09c8a60d158cac91499c35ec1633f75709121013
Mohsin Zaheer <mohsinzaheer25@hotmail.com>
2026-03-27 11:24:13 -0400

    Update Homebrew installation docs

--- homebrew/kubectx-bar.rb
+++ homebrew/kubectx-bar.rb
@@ -1,26 +1,20 @@
-# Homebrew Cask for KubeCtx
-# 
-# To use this cask, you need to create a tap repository:
-# 1. Create a repo named "homebrew-tap" on GitHub
-# 2. Add this file as Casks/kubectx-bar.rb
-# 3. Users can then install via: brew tap mohsinzaheer25/tap && brew install --cask kubectx-bar
-#
-# For releases, create a GitHub release with a zip file of KubeCtx.app
-
 cask "kubectx-bar" do
   version "1.0.0"
-  sha256 :no_check  # Update this with actual SHA256 when you create releases
+  sha256 "922f2a3f15a4cd208fdff830cddd033719275fa41fb8debc86235baf9dd4882c"
 
-  url "https://github.com/mohsinzaheer25/KubeCtxBar/releases/download/v#{version}/KubeCtx.app.zip"
+  url "https://github.com/mohsinzaheer25/KubeCtxBar/releases/download/v#{version}/KubeCtx-#{version}.zip"
   name "KubeCtx"
-  desc "Kubernetes context switcher for macOS menu bar"
+  desc "Kubernetes context switcher for the menu bar"
   homepage "https://github.com/mohsinzaheer25/KubeCtxBar"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   depends_on macos: ">= :sonoma"
 
   app "KubeCtx.app"
 
-  zap trash: [
-    "~/Library/Preferences/com.kubectx.app.plist",
-  ]
+  zap trash: "~/Library/Preferences/com.kubectx.app.plist"
 end
